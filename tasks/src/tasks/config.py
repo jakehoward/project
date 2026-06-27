ca5ca5c3d51b4954adfb928bd4ae0771ea8a141d
Jake Howard <jake@jakehoward.org>
2026-06-27 12:30:59 +0100

    Use JSON for config rather than TOML because goal is to be zero deps
    - and python has a json read/write but only toml read.

--- tasks/src/tasks/config.py
+++ tasks/src/tasks/config.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-import tomllib
-from dataclasses import dataclass
+import json
+from dataclasses import asdict, dataclass
 from pathlib import Path
 
-CONFIG_FILENAME = ".tasks.toml"
+CONFIG_FILENAME = ".tasks.json"
 
 
 @dataclass(frozen=True)
@@ -12,18 +12,10 @@ class Config:
     tasks_dir: Path
 
 
-def _assert_tasks_dir_path_is_valid(tasks_dir: Path) -> None:
-    path_string = str(tasks_dir)
-    if '"' in path_string:
-        raise ValueError(
-            f"Invalid Config: 'tasks_dir' path cannot contain double quote: {path_string}"
-        )
-
-
 def read_config() -> Config | None:
     if Path(CONFIG_FILENAME).exists():
-        with open(CONFIG_FILENAME, "rb") as f:
-            config = tomllib.load(f)
+        with open(CONFIG_FILENAME) as f:
+            config = json.load(f)
 
         if "tasks_dir" not in config:
             raise ValueError(f"Invalid Config: 'tasks_dir' is missing in {CONFIG_FILENAME}")
@@ -32,7 +24,6 @@ def read_config() -> Config | None:
                 "Config tasks_dir invalid, must be inside the current working directory"
             )
 
-        _assert_tasks_dir_path_is_valid(Path(config["tasks_dir"]))
         return Config(tasks_dir=Path(config["tasks_dir"]))
     return None
 
@@ -42,9 +33,8 @@ def get_config_path() -> Path:
 
 
 def write_config(config: Config) -> None:
-    _assert_tasks_dir_path_is_valid(config.tasks_dir)
     with open(CONFIG_FILENAME, "w") as f:
-        f.write(f'tasks_dir = "{config.tasks_dir}"\n')
+        json.dump(asdict(config), f, default=str)
 
 
 def assert_initialised() -> None:
--- tasks/tests/commands/test_config.py
+++ tasks/tests/commands/test_config.py
@@ -1,4 +1,4 @@
-import tomllib
+import json
 from pathlib import Path
 
 import pytest
@@ -15,7 +15,7 @@ def test_write_config(tmp_path: Path, monkeypatch) -> None:
     assert Path(CONFIG_FILENAME).exists()
 
     with open(CONFIG_FILENAME, "rb") as f:
-        config = tomllib.load(f)
+        config = json.load(f)
     assert config == {"tasks_dir": "foo/bar/tasks"}
 
 
@@ -31,7 +31,7 @@ def test_read_config_throws_if_path_not_relative_to_cwd(tmp_path: Path, monkeypa
 
     write_config(Config(tasks_dir=Path("./foo/baz/tasks")))
     with open(Path.cwd() / CONFIG_FILENAME, "w") as f:
-        f.write('tasks_dir = "../outside"')
+        json.dump({"tasks_dir": "../outside"}, f)
 
     with pytest.raises(
         ValueError,
@@ -45,36 +45,11 @@ def test_read_config_throws_if_tasks_dir_not_present(tmp_path: Path, monkeypatch
 
     write_config(Config(tasks_dir=Path("./foo/baz/tasks")))
     with open(Path.cwd() / CONFIG_FILENAME, "w") as f:
-        f.write('nope = "foo"')
+        f.write('{"nope":"foo"}')
 
     with pytest.raises(
         ValueError,
         match=f"Invalid Config: 'tasks_dir' is missing in {CONFIG_FILENAME}",
-    ):
-        read_config()
-
-
-def test_write_throws_if_tasks_dir_contains_double_quote(tmp_path: Path, monkeypatch) -> None:
-    monkeypatch.chdir(tmp_path)
-
-    invalid_path = 'foo/baz/t"asks'
-    with pytest.raises(
-        ValueError,
-        match=f"Invalid Config: 'tasks_dir' path cannot contain double quote: {invalid_path}",
-    ):
-        write_config(Config(tasks_dir=Path(invalid_path)))
-
-
-def test_read_throws_if_tasks_dir_contains_double_quote(tmp_path: Path, monkeypatch) -> None:
-    monkeypatch.chdir(tmp_path)
-
-    write_config(Config(tasks_dir=Path("./foo/baz/tasks")))
-    invalid_path = 'foo/baz/t\\"asks'
-    with open(Path.cwd() / CONFIG_FILENAME, "w") as f:
-        f.write(f'"tasks_dir" = "{invalid_path}"')
-
-    with pytest.raises(
-        ValueError, match="Invalid Config: 'tasks_dir' path cannot contain double quote"
     ):
         read_config()
 
